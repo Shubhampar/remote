b28892d20d244d56bd1afe92987f73f7a4b83f56
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-12-16 23:19:07 +0530

    push

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AllRoutes from './components/AllRoutes';
+import Navbar from './components/Navbar';
+import "./index.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
